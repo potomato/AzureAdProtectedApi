f266a60e8b6c938e15dac5219f92a883d1cc3d1b
tom.abnett <tom.abnett@correla.com>
2023-01-03 12:51:05 +0000

    Added anonymous controller and dockerfile

---
+++ Controllers/AnonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tokenauth.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnonController : ControllerBase
+{
+    // GET: api/<AnonController>
+    [HttpGet]
+    public string Get()
+    {
+        return "Anonymous request succeeded";
+    }
+}
