be0ae46082ca4db00b9aa68143e3bdeaa80f5b69
tom.abnett <tom.abnett@correla.com>
2023-01-03 10:05:35 +0000

    Changes for prototype

---
+++ Controllers/OrgController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tokenauth.Controllers;
+
+[Authorize(Roles = "Get.Keys")]
+[ApiController]
+[Route("api/[controller]")]
+public class OrgController : ControllerBase
+{
+    // GET: api/<OrgController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return User.Identities.Single().Claims.Select(c => $"{c.Type}={c.Value}");
+    }
+}
