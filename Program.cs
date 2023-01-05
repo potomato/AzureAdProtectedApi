7534c853716ac63f39774992e2388a92a078e1a5
tom.abnett <tom.abnett@correla.com>
2023-01-05 09:47:14 +0000

    Enable swagger in production

--- Program.cs
+++ Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddSwaggerGen(setup =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
